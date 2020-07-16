de49c03f32ae6eb634c1c405b8e858d46c5a84b6
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-16 16:43:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,3 +8,8 @@ def using_unshift (array, string)
   bouroughs_in_nyc.unshift "Staten Island"
 
 end
+
+def using_pop (array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+end
